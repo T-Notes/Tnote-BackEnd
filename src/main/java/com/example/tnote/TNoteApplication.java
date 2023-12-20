6d8c428685ee1210b0a5732e3df231d7f70bac12
hwan2-99 <thh1233123@gmail.com>
2023-12-20 16:41:50 +0900

    feat:gradle 의존성 추가

---
+++ src/main/java/com/example/tnote/TNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.tnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tnote/TNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
