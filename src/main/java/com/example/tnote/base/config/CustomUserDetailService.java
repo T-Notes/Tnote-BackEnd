44d1921cb2ddd0627825caa2b2770386942710be
hwan2-99 <thh1233123@gmail.com>
2023-12-28 17:19:27 +0900

    refactor: CustomUserDetailService loadUserByName메소드 반환 타입수정

--- src/main/java/com/example/tnote/base/config/CustomUserDetailService.java
+++ src/main/java/com/example/tnote/base/config/CustomUserDetailService.java
@@ -1,6 +1,9 @@
 package com.example.tnote.base.config;
 
 import com.example.tnote.base.exception.UserException;
+import com.example.tnote.base.response.Result;
+import com.example.tnote.boundedContext.user.entity.User;
+import com.example.tnote.boundedContext.user.entity.auth.PrincipalDetails;
 import com.example.tnote.boundedContext.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +17,17 @@ import static com.example.tnote.base.exception.UserErrorResult.USER_NOT_FOUND;
 @RequiredArgsConstructor
 @Slf4j
 @Component
-public class  CustomUserDetailService implements UserDetailsService {
+public class CustomUserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return (UserDetails) userRepository.findByEmail(email)
-                .orElseThrow(() -> new UserException(USER_NOT_FOUND));
+    public PrincipalDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // todo 예외처리 custom 해야합니다.
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("존재하는 유저가없어요"));
+        log.info("loadUserByUsername, user=[{}]", user.getEmail());
+        return new PrincipalDetails(user);
     }
 
 }
