7556dd1c2510a0f85e169ecc41109083d0ab3ee9
hwan2-99 <thh1233123@gmail.com>
2024-01-03 21:17:41 +0900

    feat: 업무일지 작성 dto 클래스 구현

--- src/main/java/com/example/tnote/boundedContext/proceeding/entity/Proceeding.java
+++ src/main/java/com/example/tnote/boundedContext/proceeding/entity/Proceeding.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name="PROCEEDING_TB")
 public class Proceeding extends BaseTimeEntity {
     @Id
