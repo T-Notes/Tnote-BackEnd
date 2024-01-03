bc706c80874a25915f44069bdefceb89d4e4c3b8
hwan2-99 <thh1233123@gmail.com>
2024-01-03 18:51:39 +0900

    feat: 업무일지 관리 레포지토리 구현

---
+++ src/main/java/com/example/tnote/boundedContext/proceeding/repository/ProceedingRepository.java
@@ -0,0 +1,8 @@
+package com.example.tnote.boundedContext.proceeding.repository;
+
+import com.example.tnote.boundedContext.proceeding.entity.Proceeding;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProceedingRepository extends JpaRepository<Proceeding, Long> {
+
+}
