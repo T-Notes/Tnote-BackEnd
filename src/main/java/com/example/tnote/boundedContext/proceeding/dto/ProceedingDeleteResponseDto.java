9ec82a060d854b874ef1887f95e94c89c85aea9c
hwan2-99 <thh1233123@gmail.com>
2024-01-05 12:28:52 +0900

    feat: 학급일지 삭제시 응답 dto 클래스 구현

---
+++ src/main/java/com/example/tnote/boundedContext/proceeding/dto/ProceedingDeleteResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.tnote.boundedContext.proceeding.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProceedingDeleteResponseDto {
+    private Long id;
+}
