47245c8372691e3022230ec68bd4e01bac1fba93
hwan2-99 <thh1233123@gmail.com>
2024-01-05 13:00:30 +0900

    refactor: 유저별 학급일지 전체 조회 쿼리문 수정

--- src/main/java/com/example/tnote/boundedContext/classLog/repository/ClassLogRepository.java
+++ src/main/java/com/example/tnote/boundedContext/classLog/repository/ClassLogRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ClassLogRepository extends JpaRepository<ClassLog, Long> {
-    @Query("select cl from ClassLog cl " +
-            "where cl.id = :classLogId and cl.user.id = :userId")
-    Optional<ClassLog> findByIdAndUserId(Long classLogId, Long userId);
+    @Query("select cl from ClassLog cl where cl.user.id = :userId")
+    List<ClassLog> findAllByUserId(Long userId);
+
+    Optional<ClassLog> findByIdAndUserId
+
     List<ClassLog> findAllByUserId(Long userId);
 }
