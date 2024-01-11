bb6ec4623e4e4851d212402488acef0bfe3e1f71
hwan2-99 <thh1233123@gmail.com>
2024-01-11 21:16:46 +0900

    refactor: 관찰일지 Builder 어노테이션 추가

--- src/main/java/com/example/tnote/boundedContext/observation/entity/Observation.java
+++ src/main/java/com/example/tnote/boundedContext/observation/entity/Observation.java
@@ -6,13 +6,13 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
 @Table(name="OBSERVATION_TB")
 public class Observation extends BaseTimeEntity {
     @Id
