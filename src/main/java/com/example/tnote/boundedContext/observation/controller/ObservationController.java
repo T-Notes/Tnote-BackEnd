d366d1abc1fabf9b1dc64f8da80a6b64d3a9388b
hwan2-99 <thh1233123@gmail.com>
2024-01-11 21:25:14 +0900

    feat: 관찰일지 작성 컨트롤러 구현

--- src/main/java/com/example/tnote/boundedContext/observation/controller/ObservationController.java
+++ src/main/java/com/example/tnote/boundedContext/observation/controller/ObservationController.java
@@ -1,7 +1,17 @@
 package com.example.tnote.boundedContext.observation.controller;
 
+import com.example.tnote.base.response.Result;
+import com.example.tnote.boundedContext.observation.dto.ObservationRequestDto;
+import com.example.tnote.boundedContext.observation.dto.ObservationResponseDto;
+import com.example.tnote.boundedContext.observation.service.ObservationService;
+import com.example.tnote.boundedContext.user.entity.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/observation")
 @RequiredArgsConstructor
 public class ObservationController {
-
+    private final ObservationService observationService;
+    @PostMapping("/observations")
+    public ResponseEntity<Result> createObservation(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                                    @RequestBody
+                                                    ObservationRequestDto requestDto) {
+        ObservationResponseDto observationResponseDto = observationService.save(principalDetails.getId(), requestDto);
+        return ResponseEntity.ok(Result.of(observationResponseDto));
+    }
 }
