ec9c00f59ed1af5c75f7eae0c12c4e62bd4f9953
hwan2-99 <thh1233123@gmail.com>
2024-01-11 21:13:36 +0900

    feat: 관찰기록 응답 dto 클래스 구현

---
+++ src/main/java/com/example/tnote/boundedContext/observation/dto/ObservationResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.tnote.boundedContext.observation.dto;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ObservationResponseDto {
+    private Long id;
+    private String studentName;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String observationContents; // 관찰 내용
+    private String guidance; // 해석 및 지도 방안
+}
