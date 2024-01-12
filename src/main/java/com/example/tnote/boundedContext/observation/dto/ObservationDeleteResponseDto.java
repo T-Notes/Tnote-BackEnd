fa905ea3c3b49eb86a77dad63a4aa0a9aba723ea
hwan2-99 <thh1233123@gmail.com>
2024-01-12 15:24:04 +0900

    feat: 관찰일지 삭제 응답 dto 클래스 구현

---
+++ src/main/java/com/example/tnote/boundedContext/observation/dto/ObservationDeleteResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.tnote.boundedContext.observation.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ObservationDeleteResponseDto {
+    private Long id;
+}
