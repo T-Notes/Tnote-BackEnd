90379280fdd3758803dffa0d801a6760e8273920
hwan2-99 <thh1233123@gmail.com>
2024-01-13 12:02:53 +0900

    feat: 관찰일지 이미지 엔티티 구현

---
+++ src/main/java/com/example/tnote/boundedContext/observation/entity/ObservationImage.java
@@ -0,0 +1,46 @@
+package com.example.tnote.boundedContext.observation.entity;
+
+import com.example.tnote.boundedContext.classLog.entity.ClassLog;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ObservationImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "observation_image_id")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String observationImageUrl;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "classLog_id")
+    private Observation observation;
+
+    @Builder
+    public ObservationImage(Observation observation, String observationImageUrl) {
+        this.observation = observation;
+        if (observation != null) {
+            observation.getObservationImage().add(this);
+        }
+        this.observationImageUrl = observationImageUrl;
+    }
+}
