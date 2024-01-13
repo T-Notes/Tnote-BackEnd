4ae83c45de7a7bc8ed8a816b8d0dfc9f974bb6c0
hwan2-99 <thh1233123@gmail.com>
2024-01-13 11:27:59 +0900

    feat: 상담기록 이미지 업로드를 이미지 관리 엔티티 구현

---
+++ src/main/java/com/example/tnote/boundedContext/consultation/entity/ConsultationImage.java
@@ -0,0 +1,46 @@
+package com.example.tnote.boundedContext.consultation.entity;
+
+import com.example.tnote.boundedContext.classLog.entity.ClassLog;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ConsultationImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "consultation_image_id")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String consultationImageUrl;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "consultation_id")
+    private Consultation consultation;
+
+    @Builder
+    public ConsultationImage(Consultation consultation, String consultationImageUrl) {
+        this.consultation = consultation;
+        if (consultation != null) {
+            consultation.getConsultationImage().add(this);
+        }
+        this.consultationImageUrl = consultationImageUrl;
+    }
+}
