11a0a860031d8bb577855a86fd053bb4f1c35a73
hwan2-99 <thh1233123@gmail.com>
2024-01-15 13:15:42 +0900

    feat: 상담일지 작성 요청 dto 클래스에 entity변환 메소드 구현

--- src/main/java/com/example/tnote/boundedContext/consultation/dto/ConsultationRequestDto.java
+++ src/main/java/com/example/tnote/boundedContext/consultation/dto/ConsultationRequestDto.java
@@ -1,7 +1,10 @@
 package com.example.tnote.boundedContext.consultation.dto;
 
+import com.example.tnote.base.utils.DateUtils;
+import com.example.tnote.boundedContext.consultation.entity.Consultation;
 import com.example.tnote.boundedContext.consultation.entity.CounselingField;
 import com.example.tnote.boundedContext.consultation.entity.CounselingType;
+import com.example.tnote.boundedContext.user.entity.User;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
@@ -42,6 +45,18 @@ public class ConsultationRequestDto {
             return Arrays.stream(enumClass.getEnumConstants())
                     .noneMatch(e -> e.name().equals(enumName));
         }
+    }
+    public Consultation toEntity(User user) {
+        return Consultation.builder()
+                .user(user)
+                .studentName(this.studentName)
+                .startDate(DateUtils.adjustStartDateTime(this.startDate,this.isAllDay))
+                .endDate(DateUtils.adjustEndDateTime(this.startDate,this.isAllDay))
+                .counselingField(this.counselingField)
+                .counselingType(this.counselingType)
+                .consultationContents(this.consultationContents)
+                .consultationResult(this.consultationResult)
+                .build();
     }
 
 
