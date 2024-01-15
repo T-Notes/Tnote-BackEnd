afb24621e903b6fd1d7d67acc243dc68294357b6
hwan2-99 <thh1233123@gmail.com>
2024-01-15 13:04:15 +0900

    feat: 업무일지 작성 요청 dto 클래스에 entity변환 메소드 구현

--- src/main/java/com/example/tnote/boundedContext/proceeding/dto/ProceedingRequestDto.java
+++ src/main/java/com/example/tnote/boundedContext/proceeding/dto/ProceedingRequestDto.java
@@ -1,7 +1,10 @@
 package com.example.tnote.boundedContext.proceeding.dto;
 
+import com.example.tnote.base.utils.DateUtils;
 import com.example.tnote.boundedContext.proceeding.entity.Proceeding;
+import com.example.tnote.boundedContext.user.entity.User;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,13 +20,15 @@ public class ProceedingRequestDto {
     private String workContents;
     private boolean isAllDay;
 
-    public Proceeding toEntity() {
+    public Proceeding toEntity(User user) {
         return Proceeding.builder()
-                .title(title)
-                .startDate(startDate)
-                .endDate(endDate)
-                .location(location)
-                .workContents(workContents)
+                .user(user)
+                .title(this.title)
+                .startDate(DateUtils.adjustStartDateTime(startDate, isAllDay))
+                .endDate(DateUtils.adjustEndDateTime(endDate, isAllDay))
+                .location(this.location)
+                .workContents(this.workContents)
+                .proceedingImage(new ArrayList<>())
                 .build();
     }
 }
