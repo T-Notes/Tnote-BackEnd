9182c095299c029085ad19ddfd6611b2998bc525
hwan2-99 <thh1233123@gmail.com>
2024-01-15 20:09:17 +0900

    feat: 날짜별 일지 리스트 불러오기 기능 구현

--- src/main/java/com/example/tnote/boundedContext/home/service/HomeService.java
+++ src/main/java/com/example/tnote/boundedContext/home/service/HomeService.java
@@ -2,12 +2,21 @@ package com.example.tnote.boundedContext.home.service;
 
 import com.example.tnote.base.exception.UserErrorResult;
 import com.example.tnote.base.exception.UserException;
+import com.example.tnote.boundedContext.classLog.dto.ClassLogResponseDto;
+import com.example.tnote.boundedContext.classLog.service.ClassLogService;
 import com.example.tnote.boundedContext.consultation.dto.ConsultationResponseDto;
 import com.example.tnote.boundedContext.consultation.entity.Consultation;
+import com.example.tnote.boundedContext.consultation.service.ConsultationService;
+import com.example.tnote.boundedContext.home.dto.ArchiveResponseDto;
 import com.example.tnote.boundedContext.home.repository.ConsultationQueryRepository;
+import com.example.tnote.boundedContext.observation.dto.ObservationResponseDto;
+import com.example.tnote.boundedContext.observation.service.ObservationService;
+import com.example.tnote.boundedContext.proceeding.dto.ProceedingResponseDto;
+import com.example.tnote.boundedContext.proceeding.service.ProceedingService;
 import com.example.tnote.boundedContext.user.entity.User;
 import com.example.tnote.boundedContext.user.entity.auth.PrincipalDetails;
 import com.example.tnote.boundedContext.user.repository.UserRepository;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +31,10 @@ public class HomeService {
     private final ConsultationQueryRepository consultationQueryRepository;
     // private final ObservationQueryRepository observationQueryRepository;
     private final UserRepository userRepository;
-
+    private final ClassLogService classLogService;
+    private final ProceedingService proceedingService;
+    private final ConsultationService consultationService;
+    private final ObservationService observationService;
     public List<ConsultationResponseDto> findAllOfConsultation(String studentName, PrincipalDetails user) {
 
         if (!checkCurrentUser(user.getId())) {
@@ -53,6 +65,20 @@ public class HomeService {
         Optional<User> currentUser = userRepository.findById(id);
 
         return currentUser.isPresent();
+    }
+
+    public ArchiveResponseDto readDailyLogs(Long userId, LocalDate date) {
+        List<ClassLogResponseDto> classLogs = classLogService.readDailyClassLogs(userId, date);
+        List<ConsultationResponseDto> consultations = consultationService.readDailyConsultations(userId, date);
+        List<ObservationResponseDto> observations = observationService.readDailyObservations(userId, date);
+        List<ProceedingResponseDto> proceedings = proceedingService.readDailyProceedings(userId, date);
+
+        return ArchiveResponseDto.builder()
+                .classLogs(classLogs)
+                .consultations(consultations)
+                .observations(observations)
+                .proceedings(proceedings)
+                .build();
     }
 
 }
