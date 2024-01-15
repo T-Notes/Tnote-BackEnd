e20e08e81f7812940c76e4d9ead74204146f19c6
hwan2-99 <thh1233123@gmail.com>
2024-01-15 12:59:50 +0900

    feat: 관찰일지 작성 요청 dto 클래스에 entity변환 메소드 구현

--- src/main/java/com/example/tnote/boundedContext/observation/dto/ObservationRequestDto.java
+++ src/main/java/com/example/tnote/boundedContext/observation/dto/ObservationRequestDto.java
@@ -1,6 +1,10 @@
 package com.example.tnote.boundedContext.observation.dto;
 
+import com.example.tnote.base.utils.DateUtils;
+import com.example.tnote.boundedContext.observation.entity.Observation;
+import com.example.tnote.boundedContext.user.entity.User;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,4 +19,15 @@ public class ObservationRequestDto {
     private String observationContents; // 관찰 내용
     private String guidance; // 해석 및 지도 방안
     private boolean isAllDay;
+    public Observation toEntity(User user) {
+        return Observation.builder()
+                .user(user)
+                .studentName(this.studentName)
+                .startDate(DateUtils.adjustStartDateTime(this.startDate, this.isAllDay))
+                .endDate(DateUtils.adjustEndDateTime(this.endDate, this.isAllDay))
+                .observationContents(this.observationContents)
+                .guidance(this.guidance)
+                .observationImage(new ArrayList<>())
+                .build();
+    }
 }
