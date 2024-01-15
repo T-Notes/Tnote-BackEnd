daa877e6708104527eecaf86ffd8f07f44bf2851
hwan2-99 <thh1233123@gmail.com>
2024-01-15 19:36:52 +0900

    feat: 날짜별 업무일지 리스트 조회기능 구현

--- src/main/java/com/example/tnote/boundedContext/proceeding/repository/ProceedingRepository.java
+++ src/main/java/com/example/tnote/boundedContext/proceeding/repository/ProceedingRepository.java
@@ -2,6 +2,7 @@ package com.example.tnote.boundedContext.proceeding.repository;
 
 import com.example.tnote.boundedContext.classLog.entity.ClassLog;
 import com.example.tnote.boundedContext.proceeding.entity.Proceeding;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface ProceedingRepository extends JpaRepository<Proceeding, Long> {
     @Query("select p from Proceeding p " +
             "where p.id = :proceedingId and p.user.id = :userId")
     Optional<Proceeding> findByIdAndUserId(Long proceedingId, Long userId);
+    List<Proceeding> findByUserIdAndStartDateBetween(Long userId, LocalDateTime startOfDay, LocalDateTime endOfDay);
 }
