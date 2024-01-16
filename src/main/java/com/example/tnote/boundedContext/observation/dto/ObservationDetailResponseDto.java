e853310329806e5a689cd2943bdbca3b36aae566
hwan2-99 <thh1233123@gmail.com>
2024-01-16 17:16:18 +0900

    refactor: 관찰일지 상세조회 시 이미지 조회를 위한 필드 추가

--- src/main/java/com/example/tnote/boundedContext/observation/dto/ObservationDetailResponseDto.java
+++ src/main/java/com/example/tnote/boundedContext/observation/dto/ObservationDetailResponseDto.java
@@ -1,7 +1,9 @@
 package com.example.tnote.boundedContext.observation.dto;
 
 import com.example.tnote.boundedContext.observation.entity.Observation;
+import com.example.tnote.boundedContext.observation.entity.ObservationImage;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
@@ -13,8 +15,9 @@ public class ObservationDetailResponseDto {
     private LocalDateTime endDate;
     private String observationContents; // 관찰 내용
     private String guidance; // 해석 및 지도 방안
+    private List<String> observationImageUrls;
 
-    public ObservationDetailResponseDto(Observation observation) {
+    public ObservationDetailResponseDto(Observation observation, List<ObservationImage> observationImages) {
         this.id = observation.getId();
         this.userId = observation.getUser().getId();
         this.studentName = observation.getStudentName();
@@ -22,5 +25,6 @@ public class ObservationDetailResponseDto {
         this.endDate = observation.getEndDate();
         this.observationContents = observation.getObservationContents();
         this.guidance = observation.getGuidance();
+        this.observationImageUrls = observationImages.stream().map(ObservationImage::getObservationImageUrl).toList();
     }
 }
