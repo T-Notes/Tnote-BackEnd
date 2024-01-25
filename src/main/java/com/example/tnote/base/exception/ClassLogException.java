b814df6811a3fbb0056d8217177108b587dcf8af
hwan2-99 <thh1233123@gmail.com>
2024-01-25 19:15:34 +0900

    refactor: ClassLogErrorResult 필드 상수화

--- src/main/java/com/example/tnote/base/exception/ClassLogException.java
+++ src/main/java/com/example/tnote/base/exception/ClassLogException.java
@@ -1,5 +1,10 @@
 package com.example.tnote.base.exception;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class ClassLogException extends RuntimeException {
-    private ClassLogErrorResult classLogErrorResult;
+    private final ClassLogErrorResult classLogErrorResult;
 }
