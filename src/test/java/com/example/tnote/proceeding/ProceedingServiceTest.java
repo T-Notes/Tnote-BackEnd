9ac9d3d4f13360c3244dcdd89c345b157fe602af
hwan2-99 <thh1233123@gmail.com>
2024-01-26 13:20:55 +0900

    test: 업무일지 상세정보 조회 테스트 구현

--- src/test/java/com/example/tnote/proceeding/ProceedingServiceTest.java
+++ src/test/java/com/example/tnote/proceeding/ProceedingServiceTest.java
@@ -9,11 +9,15 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.example.tnote.base.exception.UserException;
+import com.example.tnote.boundedContext.classLog.dto.ClassLogDetailResponseDto;
 import com.example.tnote.boundedContext.classLog.dto.ClassLogResponseDto;
 import com.example.tnote.boundedContext.classLog.entity.ClassLog;
+import com.example.tnote.boundedContext.classLog.entity.ClassLogImage;
+import com.example.tnote.boundedContext.proceeding.dto.ProceedingDetailResponseDto;
 import com.example.tnote.boundedContext.proceeding.dto.ProceedingRequestDto;
 import com.example.tnote.boundedContext.proceeding.dto.ProceedingResponseDto;
 import com.example.tnote.boundedContext.proceeding.entity.Proceeding;
+import com.example.tnote.boundedContext.proceeding.entity.ProceedingImage;
 import com.example.tnote.boundedContext.proceeding.repository.ProceedingImageRepository;
 import com.example.tnote.boundedContext.proceeding.repository.ProceedingRepository;
 import com.example.tnote.boundedContext.proceeding.service.ProceedingService;
@@ -88,9 +92,9 @@ public class ProceedingServiceTest {
         verify(userRepository).findById(userId);
         verify(proceedingRepository, never()).save(any(Proceeding.class));
     }
-    @DisplayName("업무일지 조회: 작성자가 작성한 모든 업무일지 조회 확인")
+    @DisplayName("업무일지 조회: 작성자가 작성한 모든 업ㅔ일지 조회 확인")
     @Test
-    void getClassLogs() {
+    void getProceedings() {
         Long userId = 1L;
 
         Proceeding mockProceeding1 = mock(Proceeding.class);
@@ -105,5 +109,35 @@ public class ProceedingServiceTest {
                 .hasSize(2);
 
         verify(proceedingRepository).findAllByUserId(userId);
+    }
+    @DisplayName("업무일지 상세 조회: 업무일지 상세 정보 조회 확인")
+    @Test
+    void getProceedingDetails() {
+        Long userId = 1L;
+        Long proceedingId = 1L;
+
+        User mockUser = mock(User.class);
+        when(mockUser.getId()).thenReturn(userId);
+
+        Proceeding mockProceeding = mock(Proceeding.class);
+        when(mockProceeding.getId()).thenReturn(proceedingId);
+        when(mockProceeding.getUser()).thenReturn(mockUser);
+
+        ProceedingImage mockProceedingImage = mock(ProceedingImage.class);
+
+        List<ProceedingImage> mockProceedingImages = List.of(mockProceedingImage);
+
+        when(proceedingRepository.findByIdAndUserId(userId, proceedingId)).thenReturn(Optional.of(mockProceeding));
+        when(proceedingImageRepository.findProceedingImageById(proceedingId)).thenReturn(mockProceedingImages);
+
+        ProceedingDetailResponseDto result = proceedingService.getProceedingDetails(userId, proceedingId);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(proceedingId);
+        assertThat(result.getUserId()).isEqualTo(userId);
+        assertThat(result.getProceedingImageUrls()).hasSize(mockProceedingImages.size());
+
+        verify(proceedingRepository).findByIdAndUserId(userId, proceedingId);
+        verify(proceedingImageRepository).findProceedingImageById(proceedingId);
     }
 }
