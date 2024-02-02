a8151b6220aed40e71b061aaab93ef7bd6483b10
hwan2-99 <thh1233123@gmail.com>
2024-02-02 16:02:53 +0900

    feat: 상담일지 수정요청 dto 클래스에 학생이름,상담대상,상담타입,시작시간,종료시간 필드 추가

--- src/main/java/com/example/tnote/boundedContext/consultation/dto/ConsultationUpdateRequestDto.java
+++ src/main/java/com/example/tnote/boundedContext/consultation/dto/ConsultationUpdateRequestDto.java
@@ -1,11 +1,39 @@
 package com.example.tnote.boundedContext.consultation.dto;
 
+import com.example.tnote.boundedContext.consultation.entity.CounselingField;
+import com.example.tnote.boundedContext.consultation.entity.CounselingType;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
 public class ConsultationUpdateRequestDto {
+    private String studentName;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private CounselingField counselingField;
+    private CounselingType counselingType;
     private String consultationContents;
     private String consultationResult;
+
+    public boolean hasStudentName() {
+        return studentName != null;
+    }
+
+    public boolean hasStartDate() {
+        return startDate != null;
+    }
+
+    public boolean hasEndDate() {
+        return endDate != null;
+    }
+
+    public boolean hasCounselingField() {
+        return counselingField != null;
+    }
+
+    public boolean hasCounselingType() {
+        return counselingType != null;
+    }
 
     public boolean hasConsultationContents() {
         return consultationContents != null;
