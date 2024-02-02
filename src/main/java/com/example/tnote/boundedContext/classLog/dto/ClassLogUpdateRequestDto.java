46d94cee8ee2b6386947590e77b67e5f2539c18d
hwan2-99 <thh1233123@gmail.com>
2024-02-02 15:07:21 +0900

    refactor: 학급일지 수정요청 dto 클래스에 제목,시작시간,종료시간 필드 추가

--- src/main/java/com/example/tnote/boundedContext/classLog/dto/ClassLogUpdateRequestDto.java
+++ src/main/java/com/example/tnote/boundedContext/classLog/dto/ClassLogUpdateRequestDto.java
@@ -1,16 +1,29 @@
 package com.example.tnote.boundedContext.classLog.dto;
 
-import java.util.List;
+import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class ClassLogUpdateRequestDto {
+    private String title;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
     private String plan;
     private String classContents;
     private String submission;
     private String magnitude;
 
+    public boolean hasTitle() {
+        return title != null;
+    }
+
+    public boolean hasStartDate() {
+        return startDate != null;
+    }
+
+    public boolean hasEndDate() {
+        return endDate != null;
+    }
     public boolean hasPlan() {
         return plan != null;
     }
