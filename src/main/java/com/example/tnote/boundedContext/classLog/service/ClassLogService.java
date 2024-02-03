be50be0ee07d9e8138b3ed2c36b1ffc909e43f5c
hwan2-99 <thh1233123@gmail.com>
2024-02-03 09:46:38 +0900

    refactor: 학급일지 조회시 페이징 기능 추가

--- src/main/java/com/example/tnote/boundedContext/classLog/service/ClassLogService.java
+++ src/main/java/com/example/tnote/boundedContext/classLog/service/ClassLogService.java
@@ -10,6 +10,7 @@ import com.example.tnote.boundedContext.classLog.dto.ClassLogDeleteResponseDto;
 import com.example.tnote.boundedContext.classLog.dto.ClassLogDetailResponseDto;
 import com.example.tnote.boundedContext.classLog.dto.ClassLogRequestDto;
 import com.example.tnote.boundedContext.classLog.dto.ClassLogResponseDto;
+import com.example.tnote.boundedContext.classLog.dto.ClassLogSliceResponseDto;
 import com.example.tnote.boundedContext.classLog.dto.ClassLogUpdateRequestDto;
 import com.example.tnote.boundedContext.classLog.entity.ClassLog;
 import com.example.tnote.boundedContext.classLog.entity.ClassLogImage;
@@ -23,6 +24,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -63,13 +66,18 @@ public class ClassLogService {
 
 
     @Transactional(readOnly = true)
-    public List<ClassLogResponseDto> readAllClassLog(Long userId) {
-        //todo slice 형태로 바꿔야합니다
+    public ClassLogSliceResponseDto readAllClassLog(Long userId, Pageable pageable) {
         List<ClassLog> classLogs = classLogRepository.findAllByUserId(userId);
+        Slice<ClassLog> allClassLogsSlice = classLogRepository.findAllBy(pageable);
+        int numberOfClassLog = classLogs.size();
+        List<ClassLogResponseDto> classLogResponseDtos = allClassLogsSlice.getContent().stream().map(ClassLogResponseDto::of).toList();
 
-        return classLogs.stream()
-                .map(ClassLogResponseDto::of)
-                .toList();
+        return ClassLogSliceResponseDto.builder()
+                .classLogs(classLogResponseDtos)
+                .numberOfClassLog(numberOfClassLog)
+                .page(allClassLogsSlice.getPageable().getPageNumber())
+                .isLast(allClassLogsSlice.isLast())
+                .build();
     }
 
     @Transactional(readOnly = true)
