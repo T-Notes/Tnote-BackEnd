1fc934f89a168df83309555f9593e68dfa9ea68c
hwan2-99 <thh1233123@gmail.com>
2024-02-07 17:57:40 +0900

    refactor: 아카이브 응답 dto 클래스 페이징처리를 한 dto를 필드로 변경 및 정적 팩토리 메소드 구현

--- src/main/java/com/example/tnote/boundedContext/home/dto/ArchiveResponseDto.java
+++ src/main/java/com/example/tnote/boundedContext/home/dto/ArchiveResponseDto.java
@@ -1,9 +1,13 @@
 package com.example.tnote.boundedContext.home.dto;
 
 import com.example.tnote.boundedContext.classLog.dto.ClassLogResponseDto;
+import com.example.tnote.boundedContext.classLog.dto.ClassLogSliceResponseDto;
 import com.example.tnote.boundedContext.consultation.dto.ConsultationResponseDto;
+import com.example.tnote.boundedContext.consultation.dto.ConsultationSliceResponseDto;
 import com.example.tnote.boundedContext.observation.dto.ObservationResponseDto;
+import com.example.tnote.boundedContext.observation.dto.ObservationSliceResponseDto;
 import com.example.tnote.boundedContext.proceeding.dto.ProceedingResponseDto;
+import com.example.tnote.boundedContext.proceeding.dto.ProceedingSliceResponseDto;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,13 +15,13 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ArchiveResponseDto {
-    private List<ClassLogResponseDto> classLogs;
-    private List<ObservationResponseDto> observations;
-    private List<ConsultationResponseDto> consultations;
-    private List<ProceedingResponseDto> proceedings;
+    private List<ClassLogSliceResponseDto> classLogs;
+    private List<ObservationSliceResponseDto> observations;
+    private List<ConsultationSliceResponseDto> consultations;
+    private List<ProceedingSliceResponseDto> proceedings;
 
-    public ArchiveResponseDto of(List<ClassLogResponseDto> classLogs, List<ObservationResponseDto> observations,
-                                 List<ConsultationResponseDto> consultations, List<ProceedingResponseDto> proceedings) {
+    public static ArchiveResponseDto of(List<ClassLogSliceResponseDto> classLogs, List<ObservationSliceResponseDto> observations,
+                                 List<ConsultationSliceResponseDto> consultations, List<ProceedingSliceResponseDto> proceedings) {
         return ArchiveResponseDto.builder()
                 .classLogs(classLogs)
                 .consultations(consultations)
