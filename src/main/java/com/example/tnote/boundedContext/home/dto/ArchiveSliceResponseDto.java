8488463b8a0a29b5a259d180efad59ed5ef9033b
hwan2-99 <thh1233123@gmail.com>
2024-02-08 16:36:46 +0900

    refactor: 아카이브 페이징 응답 dto 클래스명 변경

--- src/main/java/com/example/tnote/boundedContext/home/dto/ArchiveSliceResponseDto.java
+++ src/main/java/com/example/tnote/boundedContext/home/dto/ArchiveSliceResponseDto.java
@@ -1,21 +1,16 @@
 package com.example.tnote.boundedContext.home.dto;
 
-import com.example.tnote.boundedContext.classLog.dto.ClassLogResponseDto;
 import com.example.tnote.boundedContext.classLog.dto.ClassLogSliceResponseDto;
-import com.example.tnote.boundedContext.consultation.dto.ConsultationResponseDto;
 import com.example.tnote.boundedContext.consultation.dto.ConsultationSliceResponseDto;
-import com.example.tnote.boundedContext.observation.dto.ObservationResponseDto;
 import com.example.tnote.boundedContext.observation.dto.ObservationSliceResponseDto;
-import com.example.tnote.boundedContext.proceeding.dto.ProceedingResponseDto;
 import com.example.tnote.boundedContext.proceeding.dto.ProceedingSliceResponseDto;
 import com.example.tnote.boundedContext.todo.dto.TodoSliceResponseDto;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-public class ArchiveResponseDto {
+public class ArchiveSliceResponseDto {
     private ClassLogSliceResponseDto classLogs;
     private ObservationSliceResponseDto observations;
     private ConsultationSliceResponseDto consultations;
