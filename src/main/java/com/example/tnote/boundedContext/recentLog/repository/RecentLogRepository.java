3cfd24dce88cc118dd289f3bda7cfe3c7ac40dd0
hwan2-99 <thh1233123@gmail.com>
2024-02-15 22:41:11 +0900

    feat: 최근 조회한 일지정보 db에서 호출 기능 구현

--- src/main/java/com/example/tnote/boundedContext/recentLog/repository/RecentLogRepository.java
+++ src/main/java/com/example/tnote/boundedContext/recentLog/repository/RecentLogRepository.java
@@ -1,7 +1,10 @@
 package com.example.tnote.boundedContext.recentLog.repository;
 
 import com.example.tnote.boundedContext.recentLog.entity.RecentLog;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecentLogRepository extends JpaRepository<RecentLog, Long> {
+    List<RecentLog> findTop4ByUserIdOrderByTimestampDesc(Long userId);
+
 }
