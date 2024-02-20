6cb156203affe9ef03fe5e6e317f0807867a1b00
hwan2-99 <thh1233123@gmail.com>
2024-02-20 14:08:49 +0900

    refactor: cors 처리를 위한 주소 추가

--- src/main/java/com/example/tnote/base/config/WebConfig.java
+++ src/main/java/com/example/tnote/base/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://54.180.14.127:8080")
+                .allowedOrigins("http://localhost:3000", "http://54.180.14.127:8080")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "OPTIONS", "DELETE");
     }
 }
