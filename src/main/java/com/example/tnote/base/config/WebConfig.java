bfc9bd5cce2b612f75e59a1c8290860560f2e45f
hwan2-99 <thh1233123@gmail.com>
2024-02-20 13:43:23 +0900

    refactor: cors 처리를 위한 주소 변경

--- src/main/java/com/example/tnote/base/config/WebConfig.java
+++ src/main/java/com/example/tnote/base/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://54.180.14.12:8080")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "OPTIONS", "DELETE");
     }
 }
