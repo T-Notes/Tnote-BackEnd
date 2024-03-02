1fc18d2f0e26379009527b26ef4087bbef8db173
j9972 <j9972@naver.com>
2024-03-02 21:32:17 +0900

    feat : refresh token expire 하다면 재로그인 요청

--- src/main/java/com/example/tnote/boundedContext/user/service/AuthService.java
+++ src/main/java/com/example/tnote/boundedContext/user/service/AuthService.java
@@ -51,9 +51,8 @@ public class AuthService {
             return buildSignInResponse(newToken.getAccessToken(), refreshToken, user.getId());
 
         } else {
-            refreshTokenService.save(newToken.getRefreshToken(), user.getEmail());
-
-            return buildSignInResponse(newToken.getAccessToken(), newToken.getRefreshToken(), user.getId());
+            // refresh token 만료시 재로그인 필요
+            throw new JwtException(JwtErrorResult.EXPIRED_REFRESH_TOKEN);
         }
     }
 
