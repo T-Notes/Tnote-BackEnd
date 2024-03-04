7a1d19909d3dc7503f108f52777aa6b831dc6791
hwan2-99 <thh1233123@gmail.com>
2024-03-04 22:42:57 +0900

    refactor: Jwt 관련 예외처리 필터클래스 정보 수정

--- src/main/java/com/example/tnote/base/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/tnote/base/filter/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
