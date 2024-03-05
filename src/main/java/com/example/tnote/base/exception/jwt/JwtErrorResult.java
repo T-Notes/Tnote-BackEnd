8f6f6e0938dadf0556a43780917bef02dbdeb505
j9972 <j9972@naver.com>
2024-03-05 11:34:59 +0900

    fix : token 유효시간 재설정, refresh가 access 대체되는거 방지

--- src/main/java/com/example/tnote/base/exception/jwt/JwtErrorResult.java
+++ src/main/java/com/example/tnote/base/exception/jwt/JwtErrorResult.java
@@ -11,10 +11,10 @@ public enum JwtErrorResult {
     WRONG_TOKEN(HttpStatus.UNAUTHORIZED, "wrong token"),
     WRONG_SIGNATURE(HttpStatus.UNAUTHORIZED, "wrong signature"),
     UNSUPPORTED(HttpStatus.UNAUTHORIZED, "unsupported token"),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "expired token"),
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "expired access token"),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "expired access token"),
     NOT_FOUND_TOKEN(HttpStatus.UNAUTHORIZED, "not found token"),
+    NOT_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "this is not access token but refresh token"),
     NOT_EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "token is not expired");
 
     private final HttpStatus httpStatus;
