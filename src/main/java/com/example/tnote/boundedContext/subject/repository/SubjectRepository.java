5f8a21ccd00d79c94ff7908011ae576efb1e83d7
j9972 <j9972@naver.com>
2024-03-14 21:11:55 +0900

    fix: subject - none userID

--- src/main/java/com/example/tnote/boundedContext/subject/repository/SubjectRepository.java
+++ src/main/java/com/example/tnote/boundedContext/subject/repository/SubjectRepository.java
@@ -4,5 +4,4 @@ import com.example.tnote.boundedContext.subject.entity.Subjects;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubjectRepository extends JpaRepository<Subjects, Long> {
-    void deleteAllByUserId(Long userId);
 }
