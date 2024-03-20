5fde8b13945620bbe54f1a0b6ea4fc508824f6fb
hwan2-99 <thh1233123@gmail.com>
2024-03-21 00:41:57 +0900

    fix: 월별 업무일지 조회 조건 수정

--- src/main/java/com/example/tnote/boundedContext/proceeding/repository/ProceedingRepository.java
+++ src/main/java/com/example/tnote/boundedContext/proceeding/repository/ProceedingRepository.java
@@ -45,9 +45,11 @@ public interface ProceedingRepository extends JpaRepository<Proceeding, Long> {
     @Query("SELECT p FROM Proceeding p " +
             "WHERE p.user.id = :userId " +
             "AND p.schedule.id = :scheduleId " +
-            "AND (FUNCTION('YEAR', p.startDate) = FUNCTION('YEAR', :date) AND FUNCTION('MONTH', p.startDate) = FUNCTION('MONTH', :date)) "
-            +
-            "OR (FUNCTION('YEAR', p.endDate) = FUNCTION('YEAR', :date) AND FUNCTION('MONTH', p.endDate) = FUNCTION('MONTH', :date))")
+            "AND ((" +
+            "FUNCTION('YEAR', p.startDate) = FUNCTION('YEAR', :date) AND FUNCTION('MONTH', p.startDate) = FUNCTION('MONTH', :date)" +
+            ") OR (" +
+            "FUNCTION('YEAR', p.endDate) = FUNCTION('YEAR', :date) AND FUNCTION('MONTH', p.endDate) = FUNCTION('MONTH', :date)" +
+            "))")
     List<Proceeding> findByUserIdAndScheduleIdAndYearMonth(
             Long userId,
             Long scheduleId,
