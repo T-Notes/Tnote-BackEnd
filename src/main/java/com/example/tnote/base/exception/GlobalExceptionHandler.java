820cce9e6a7e8fc899c91a2402a056489b9e171a
j9972 <j9972@naver.com>
2024-03-22 10:47:25 +0900

    refactor : exception 처리

--- src/main/java/com/example/tnote/base/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/tnote/base/exception/GlobalExceptionHandler.java
@@ -3,6 +3,12 @@ package com.example.tnote.base.exception;
 
 import com.example.tnote.base.exception.classLog.ClassLogErrorResult;
 import com.example.tnote.base.exception.classLog.ClassLogException;
+import com.example.tnote.base.exception.common.CommonException;
+import com.example.tnote.base.exception.jwt.JwtException;
+import com.example.tnote.base.exception.schedule.ScheduleException;
+import com.example.tnote.base.exception.subject.SubjectsException;
+import com.example.tnote.base.exception.todo.TodoException;
+import com.example.tnote.base.exception.user.UserException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,60 +20,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    //    @ExceptionHandler({CommonException.class})
-//    public ResponseEntity<ErrorResponse> handleException(final CommonException exception) {
-//        log.warn("Exception occur: ", exception);
-//        return this.makeErrorResponseEntity(exception.getCommonErrorResult());
-//    }
-//
-//    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final CommonErrorResult errorResult) {
-//        return ResponseEntity.status(errorResult.getHttpStatus())
-//                .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
-//    }
-//
-//    @ExceptionHandler({JwtException.class})
-//    public ResponseEntity<ErrorResponse> handleException(final JwtException exception) {
-//        log.warn("Exception occur: ", exception);
-//        return this.makeErrorResponseEntity(exception.getJwtErrorResult());
-//    }
-//
-//    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final JwtErrorResult errorResult) {
-//        return ResponseEntity.status(errorResult.getHttpStatus())
-//                .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
-//    }
-//
-//    @ExceptionHandler({SubjectsException.class})
-//    public ResponseEntity<ErrorResponse> handleException(final SubjectsException exception) {
-//        log.warn("Exception occur: ", exception);
-//        return this.makeErrorResponseEntity(exception.getSubjectsErrorResult());
-//    }
-//
-//    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final SubjectsErrorResult errorResult) {
-//        return ResponseEntity.status(errorResult.getHttpStatus())
-//                .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
-//    }
-//
-//    @ExceptionHandler({ScheduleException.class})
-//    public ResponseEntity<ErrorResponse> handleException(final ScheduleException exception) {
-//        log.warn("Exception occur: ", exception);
-//        return this.makeErrorResponseEntity(exception.getScheduleErrorResult());
-//    }
-//
-//    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final ScheduleErrorResult errorResult) {
-//        return ResponseEntity.status(errorResult.getHttpStatus())
-//                .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
-//    }
-//
-//    @ExceptionHandler({TodoException.class})
-//    public ResponseEntity<ErrorResponse> handleException(final TodoException exception) {
-//        log.warn("Exception occur: ", exception);
-//        return this.makeErrorResponseEntity(exception.getTodoErrorResult());
-//    }
-//
-//    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final TodoErrorResult errorResult) {
-//        return ResponseEntity.status(errorResult.getHttpStatus())
-//                .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
-//    }
+    @ExceptionHandler({CommonException.class})
+    public ResponseEntity<ErrorResponse> handleException(final CommonException exception) {
+        log.warn("Exception occur: ", exception);
+        return this.makeErrorResponseEntity(exception);
+    }
+
+    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final CommonException exception) {
+        return ResponseEntity.status(exception.getHttpStatus())
+                .body(new ErrorResponse(exception.getHttpStatus().name(), exception.getMessage()));
+    }
+
+    @ExceptionHandler({JwtException.class})
+    public ResponseEntity<ErrorResponse> handleException(final JwtException exception) {
+        log.warn("Exception occur: ", exception);
+        return this.makeErrorResponseEntity(exception);
+    }
+
+    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final JwtException exception) {
+        return ResponseEntity.status(exception.getHttpStatus())
+                .body(new ErrorResponse(exception.getHttpStatus().name(), exception.getMessage()));
+    }
+
     @ExceptionHandler({ClassLogException.class})
     public ResponseEntity<ErrorResponse> handleClassLogException(final ClassLogException exception) {
         log.warn("ClassLogException occur: ", exception);
@@ -77,6 +51,51 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final ClassLogErrorResult errorResult) {
         return ResponseEntity.status(errorResult.getHttpStatus())
                 .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
+    }
+
+
+    @ExceptionHandler({SubjectsException.class})
+    public ResponseEntity<ErrorResponse> handleException(final SubjectsException exception) {
+        log.warn("Exception occur: ", exception);
+        return this.makeErrorResponseEntity(exception);
+    }
+
+    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final SubjectsException exception) {
+        return ResponseEntity.status(exception.getHttpStatus())
+                .body(new ErrorResponse(exception.getHttpStatus().name(), exception.getMessage()));
+    }
+
+    @ExceptionHandler({ScheduleException.class})
+    public ResponseEntity<ErrorResponse> handleException(final ScheduleException exception) {
+        log.warn("Exception occur: ", exception);
+        return this.makeErrorResponseEntity(exception);
+    }
+
+    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final ScheduleException exception) {
+        return ResponseEntity.status(exception.getHttpStatus())
+                .body(new ErrorResponse(exception.getHttpStatus().name(), exception.getMessage()));
+    }
+
+    @ExceptionHandler({TodoException.class})
+    public ResponseEntity<ErrorResponse> handleException(final TodoException exception) {
+        log.warn("Exception occur: ", exception);
+        return this.makeErrorResponseEntity(exception);
+    }
+
+    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final TodoException exception) {
+        return ResponseEntity.status(exception.getHttpStatus())
+                .body(new ErrorResponse(exception.getHttpStatus().name(), exception.getMessage()));
+    }
+
+    @ExceptionHandler({UserException.class})
+    public ResponseEntity<ErrorResponse> handleException(final UserException exception) {
+        log.warn("Exception occur: ", exception);
+        return this.makeErrorResponseEntity(exception);
+    }
+
+    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final UserException exception) {
+        return ResponseEntity.status(exception.getHttpStatus())
+                .body(new ErrorResponse(exception.getHttpStatus().name(), exception.getMessage()));
     }
 
 
--- src/main/java/com/example/tnote/base/exception/jwt/JwtException.java
+++ src/main/java/com/example/tnote/base/exception/jwt/JwtException.java
@@ -1,5 +1,7 @@
 package com.example.tnote.base.exception.jwt;
 
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,22 +12,23 @@ public class JwtException extends RuntimeException {
     private final HttpStatus httpStatus;
     private final String message;
 
-    public static final JwtException WRONG_TOKEN = new JwtException(HttpStatus.UNAUTHORIZED,
+
+    public static final JwtException WRONG_TOKEN = new JwtException(UNAUTHORIZED,
             "wrong token");
-    public static final JwtException WRONG_REFRESH_TOKEN = new JwtException(HttpStatus.UNAUTHORIZED,
+    public static final JwtException WRONG_REFRESH_TOKEN = new JwtException(UNAUTHORIZED,
             "wrong refresh token");
-    public static final JwtException WRONG_SIGNATURE = new JwtException(HttpStatus.UNAUTHORIZED,
+    public static final JwtException WRONG_SIGNATURE = new JwtException(UNAUTHORIZED,
             "wrong signature");
-    public static final JwtException UNSUPPORTED = new JwtException(HttpStatus.UNAUTHORIZED,
+    public static final JwtException UNSUPPORTED = new JwtException(UNAUTHORIZED,
             "unsupported token");
-    public static final JwtException EXPIRED_REFRESH_TOKEN = new JwtException(HttpStatus.UNAUTHORIZED,
+    public static final JwtException EXPIRED_REFRESH_TOKEN = new JwtException(UNAUTHORIZED,
             "expired refresh token");
-    public static final JwtException EXPIRED_ACCESS_TOKEN = new JwtException(HttpStatus.UNAUTHORIZED,
+    public static final JwtException EXPIRED_ACCESS_TOKEN = new JwtException(UNAUTHORIZED,
             "expired access token");
-    public static final JwtException NOT_FOUND_TOKEN = new JwtException(HttpStatus.UNAUTHORIZED,
+    public static final JwtException NOT_FOUND_TOKEN = new JwtException(UNAUTHORIZED,
             "not found token");
-    public static final JwtException NOT_ACCESS_TOKEN = new JwtException(HttpStatus.UNAUTHORIZED,
+    public static final JwtException NOT_ACCESS_TOKEN = new JwtException(UNAUTHORIZED,
             "this is not access token but refresh token");
-    public static final JwtException NOT_EXPIRED_TOKEN = new JwtException(HttpStatus.UNAUTHORIZED,
+    public static final JwtException NOT_EXPIRED_TOKEN = new JwtException(UNAUTHORIZED,
             "token is not expired");
 }
