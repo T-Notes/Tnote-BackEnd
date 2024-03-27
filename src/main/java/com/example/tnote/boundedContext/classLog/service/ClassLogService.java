c41ee444ba1747ac9c24cc5783fd017211e89a12
hwan2-99 <thh1233123@gmail.com>
2024-03-27 11:17:58 +0900

    refactor: 학급일지 예외처리 커스터마이징으로 수정

--- src/main/java/com/example/tnote/boundedContext/classLog/service/ClassLogService.java
+++ src/main/java/com/example/tnote/boundedContext/classLog/service/ClassLogService.java
@@ -92,7 +92,7 @@ public class ClassLogService {
     @Transactional(readOnly = true)
     public ClassLogDetailResponseDto getClassLogDetail(Long userId, Long classLogId) {
         ClassLog classLog = classLogRepository.findByIdAndUserId(classLogId, userId)
-                .orElseThrow(() -> new ClassLogException(ClassLogErrorResult.CLASS_LOG_NOT_FOUNT));
+                .orElseThrow(() -> CustomException.CLASS_LOG_NOT_FOUNT);
         List<ClassLogImage> classLogImages = classLogImageRepository.findClassLogImagesByClassLogId(classLogId);
         recentLogService.saveRecentLog(userId, classLog.getId(), "CLASS_LOG");
         return new ClassLogDetailResponseDto(classLog, classLogImages);
