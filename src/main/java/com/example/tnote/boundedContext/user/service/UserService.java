e1b5f5d94e6031876bc8453afa8922a88cf46919
j9972 <j9972@naver.com>
2024-04-03 19:59:54 +0900

    refactor : 재로그인시 user 판별

--- src/main/java/com/example/tnote/boundedContext/user/service/UserService.java
+++ src/main/java/com/example/tnote/boundedContext/user/service/UserService.java
@@ -125,6 +125,10 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public User findById(Long userId) {
-        return userRepository.findById(userId).orElseThrow(() -> CustomException.USER_NOT_FOUND);
+        User user = userRepository.findById(userId).orElseThrow(() -> CustomException.USER_NOT_FOUND);
+        if (user.getSchool().isEmpty()) {
+            throw CustomException.USER_NOT_FOUND;
+        }
+        return user;
     }
 }
