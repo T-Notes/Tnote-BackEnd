f7e4ebc5252c9023836e98d76d36294cae5b5557
j9972 <j9972@naver.com>
2024-04-17 22:51:16 +0900

    fix : user update dto 인식 안되는 문제 수정

--- src/main/java/com/example/tnote/boundedContext/user/dto/UserUpdateRequest.java
+++ src/main/java/com/example/tnote/boundedContext/user/dto/UserUpdateRequest.java
@@ -1,10 +1,14 @@
 package com.example.tnote.boundedContext.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserUpdateRequest {
 
     private String schoolName;
