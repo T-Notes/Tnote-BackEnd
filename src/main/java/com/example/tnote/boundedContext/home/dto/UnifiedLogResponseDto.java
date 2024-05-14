4419595d06dcdefc14bc2ffe4e9172b0c3993f1e
hwan2-99 <thh1233123@gmail.com>
2024-05-15 00:32:41 +0900

    feat: 서로다른 일지들을 하나의 리스트로 반환하여 반환하기 위한 응답 dto 클래스 구현

---
+++ src/main/java/com/example/tnote/boundedContext/home/dto/UnifiedLogResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.tnote.boundedContext.home.dto;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UnifiedLogResponseDto {
+    private List<LogEntry> logs;
+
+    public static UnifiedLogResponseDto from(List<LogEntry> logs) {
+        return UnifiedLogResponseDto.builder()
+                .logs(logs)
+                .build();
+    }
+}
