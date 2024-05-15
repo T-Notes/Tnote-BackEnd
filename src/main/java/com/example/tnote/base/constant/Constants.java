eb820b71c1c84af2264f305043283f4f47ce4371
j9972 <j9972@naver.com>
2024-05-15 18:44:15 +0900

    refactor : access token expire time 체크

--- src/main/java/com/example/tnote/base/constant/Constants.java
+++ src/main/java/com/example/tnote/base/constant/Constants.java
@@ -2,8 +2,8 @@ package com.example.tnote.base.constant;
 
 public class Constants {
     //public final static Long ACCESS_TOKEN_EXPIRE_COUNT = 24 * 60 * 60 * 1000L; //  1day -> 30 minutes -> 30 * 60 * 1000L
-    public final static Long ACCESS_TOKEN_EXPIRE_COUNT = 3 * 60 * 60 * 1000L; // 3시간 -test용도
-    //public final static Long ACCESS_TOKEN_EXPIRE_COUNT = 2 * 60 * 1000L; //  2분 -> test 용도
+    //public final static Long ACCESS_TOKEN_EXPIRE_COUNT = 3 * 60 * 60 * 1000L; // 3시간 -test용도
+    public final static Long ACCESS_TOKEN_EXPIRE_COUNT = 2 * 60 * 1000L; //  2분 -> test 용도
 
     public final static Long REFRESH_TOKEN_EXPIRE_COUNT = 7 * 24 * 60 * 60 * 1000L; // 7 days
 }
