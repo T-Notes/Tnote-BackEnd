fbb2ab0f6d45d1ff6f738be29a6f0c6f149eed29
hwan2-99 <thh1233123@gmail.com>
2024-05-17 01:19:32 +0900

    refactor: 필터링을 통한 로그갯수 필드추가

--- src/main/java/com/example/tnote/boundedContext/home/dto/UnifiedLogResponseDto.java
+++ src/main/java/com/example/tnote/boundedContext/home/dto/UnifiedLogResponseDto.java
@@ -8,10 +8,12 @@ import lombok.Getter;
 @Builder
 public class UnifiedLogResponseDto {
     private List<LogEntry> logs;
+    private int totalLog;
 
     public static UnifiedLogResponseDto from(List<LogEntry> logs) {
         return UnifiedLogResponseDto.builder()
                 .logs(logs)
+                .totalLog(logs.size())
                 .build();
     }
 }
