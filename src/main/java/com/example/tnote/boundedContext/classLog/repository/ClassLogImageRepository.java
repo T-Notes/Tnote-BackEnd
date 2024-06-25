0051ff5277be4266612178afd157ec66527d2598
hwan2-99 <thh1233123@gmail.com>
2024-06-26 01:12:32 +0900

    refactor: 학급일지 삭제 쿼리 수정

--- src/main/java/com/example/tnote/boundedContext/classLog/repository/ClassLogImageRepository.java
+++ src/main/java/com/example/tnote/boundedContext/classLog/repository/ClassLogImageRepository.java
@@ -1,13 +1,17 @@
 package com.example.tnote.boundedContext.classLog.repository;
 
 import com.example.tnote.boundedContext.classLog.entity.ClassLogImage;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClassLogImageRepository extends JpaRepository<ClassLogImage, Long> {
     List<ClassLogImage> findClassLogImagesByClassLogId(Long classLogId);
-
-    void deleteByClassLogId(Long classLogId);
+    @Modifying
+    @Query("DELETE FROM ClassLogImage cli WHERE cli.classLog.id = :classLogId")
+    void deleteByClassLogId(@Param("classLogId") Long classLogId);
 }
 
