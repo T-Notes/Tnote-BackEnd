79a314bb41e12f7ea1b653e3588217bd7de78db3
hwan2-99 <thh1233123@gmail.com>
2024-06-26 01:17:33 +0900

    refactor: 상담일지 이미지 삭제 쿼리 수정

--- src/main/java/com/example/tnote/boundedContext/consultation/repository/ConsultationImageRepository.java
+++ src/main/java/com/example/tnote/boundedContext/consultation/repository/ConsultationImageRepository.java
@@ -3,8 +3,12 @@ package com.example.tnote.boundedContext.consultation.repository;
 import com.example.tnote.boundedContext.consultation.entity.ConsultationImage;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ConsultationImageRepository extends JpaRepository<ConsultationImage, Long> {
     List<ConsultationImage> findConsultationImageByConsultationId(Long consultationId);
+    @Modifying
+    @Query("DELETE FROM ConsultationImage coi WHERE coi.consultation.id = :consultationId")
     void deleteByConsultationId(Long consultationId);
 }
