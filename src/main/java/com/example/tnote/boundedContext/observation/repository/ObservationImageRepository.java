9ee4e5c2822067405c74d45c11cacf5b051dd3d8
hwan2-99 <thh1233123@gmail.com>
2024-06-26 01:19:10 +0900

    refactor: 관찰일지 이미지 삭제 쿼리 수정

--- src/main/java/com/example/tnote/boundedContext/observation/repository/ObservationImageRepository.java
+++ src/main/java/com/example/tnote/boundedContext/observation/repository/ObservationImageRepository.java
@@ -3,8 +3,12 @@ package com.example.tnote.boundedContext.observation.repository;
 import com.example.tnote.boundedContext.observation.entity.ObservationImage;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ObservationImageRepository extends JpaRepository<ObservationImage, Long> {
     List<ObservationImage> findObservationImageByObservationId(Long observationId);
+    @Modifying
+    @Query("DELETE FROM ObservationImage oi WHERE oi.observation.id = :observationId")
     void deleteByObservationId(Long observationId);
 }
