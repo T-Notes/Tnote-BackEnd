7ebc2d27454b35e8b557c5b5aaf6f6a087915df5
hwan2-99 <thh1233123@gmail.com>
2024-07-08 00:48:25 +0900

    refactor: 학급일지 query 레포지토리 위치 변경

--- src/main/java/com/example/tnote/boundedContext/classLog/repository/query/ClassLogQueryRepository.java
+++ src/main/java/com/example/tnote/boundedContext/classLog/repository/query/ClassLogQueryRepository.java
@@ -1,4 +1,4 @@
-package com.example.tnote.boundedContext.archive.repository;
+package com.example.tnote.boundedContext.classLog.repository.query;
 
 import static com.example.tnote.boundedContext.classLog.entity.QClassLog.classLog;
 
