13ab7d335b9f080b029e6dedfc157ea9f7a4a09e
hwan2-99 <thh1233123@gmail.com>
2024-07-09 02:09:05 +0900

    refactor: 아카이브 관련 url 수정

--- src/main/java/com/example/tnote/boundedContext/archive/controller/ArchiveController.java
+++ src/main/java/com/example/tnote/boundedContext/archive/controller/ArchiveController.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/tnote/home")
+@RequestMapping("/tnote/archive")
 public class ArchiveController {
 
     private final ArchiveService archiveService;
