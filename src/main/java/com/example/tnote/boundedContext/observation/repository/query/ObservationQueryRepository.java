574744a32925b112e006caf5296fc9884e2cf508
hwan2-99 <thh1233123@gmail.com>
2024-07-09 01:32:48 +0900

    refactor: 관찰일지 query레포지토리 위치변경

--- src/main/java/com/example/tnote/boundedContext/observation/repository/query/ObservationQueryRepository.java
+++ src/main/java/com/example/tnote/boundedContext/observation/repository/query/ObservationQueryRepository.java
@@ -1,4 +1,4 @@
-package com.example.tnote.boundedContext.archive.repository;
+package com.example.tnote.boundedContext.observation.repository.query;
 
 import static com.example.tnote.boundedContext.observation.entity.QObservation.observation;
 
