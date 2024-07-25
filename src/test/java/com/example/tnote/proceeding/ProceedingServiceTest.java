1de3621ccf309b88bf53a7517d52b462091ee4b7
hwan2-99 <thh1233123@gmail.com>
2024-07-26 01:53:32 +0900

    test: 업무일지 테스트코드 리팩토링한 부분 수정 (#211)

--- src/test/java/com/example/tnote/proceeding/ProceedingServiceTest.java
+++ src/test/java/com/example/tnote/proceeding/ProceedingServiceTest.java
@@ -1,15 +1,12 @@
 package com.example.tnote.proceeding;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.example.tnote.base.exception.CustomExceptions;
 import com.example.tnote.boundedContext.proceeding.dto.ProceedingDeleteResponseDto;
 import com.example.tnote.boundedContext.proceeding.dto.ProceedingDetailResponseDto;
 import com.example.tnote.boundedContext.proceeding.dto.ProceedingRequestDto;
@@ -18,6 +15,7 @@ import com.example.tnote.boundedContext.proceeding.dto.ProceedingSliceResponseDt
 import com.example.tnote.boundedContext.proceeding.dto.ProceedingUpdateRequestDto;
 import com.example.tnote.boundedContext.proceeding.entity.Proceeding;
 import com.example.tnote.boundedContext.proceeding.entity.ProceedingImage;
+import com.example.tnote.boundedContext.proceeding.exception.ProceedingException;
 import com.example.tnote.boundedContext.proceeding.repository.ProceedingImageRepository;
 import com.example.tnote.boundedContext.proceeding.repository.ProceedingRepository;
 import com.example.tnote.boundedContext.proceeding.service.ProceedingService;
@@ -26,11 +24,13 @@ import com.example.tnote.boundedContext.schedule.entity.Schedule;
 import com.example.tnote.boundedContext.schedule.repository.ScheduleRepository;
 import com.example.tnote.boundedContext.user.entity.User;
 import com.example.tnote.boundedContext.user.repository.UserRepository;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -59,21 +59,38 @@ public class ProceedingServiceTest {
     @InjectMocks
     private ProceedingService proceedingService;
 
+    private User mockUser;
+    private Schedule mockSchedule;
+    private Proceeding mockProceeding;
+    private Long userId = 1L;
+    private Long scheduleId = 2L;
+    private Long proceedingId = 1L;
+
+    @BeforeEach
+    void setUp() {
+        mockUser = mock(User.class);
+        mockSchedule = new Schedule(1L, "1학기", null,
+                LocalDate.of(2024, 1, 1),
+                LocalDate.of(2024, 3, 28),
+                mockUser,
+                new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>());
+
+        mockProceeding = mock(Proceeding.class);
+    }
 
     @DisplayName("업무일지 저장: 정상적인 경우 성공적으로 저장 확인")
     @Test
     void save() {
-        Long userId = 1L;
-        Long scheduleId = 2L;
-        User mockUser = mock(User.class);
-        Schedule mockSchedule = mock(Schedule.class);
-
-        LocalDateTime now = LocalDateTime.now();
 
         ProceedingRequestDto requestDto = ProceedingRequestDto.builder()
                 .title("테스트 수업 로그")
-                .startDate(now)
-                .endDate(now.plusHours(2))
+                .startDate(mockSchedule.getStartDate().atStartOfDay())
+                .endDate(mockSchedule.getStartDate().atStartOfDay().plusHours(2))
                 .location("부산")
                 .workContents("사진찍기")
                 .isAllDay(false)
@@ -91,28 +108,9 @@ public class ProceedingServiceTest {
         verify(proceedingRepository).save(any(Proceeding.class));
     }
 
-    @DisplayName("업무일지 저장: 존재하지 않는 사용자로 인한 예외 발생 확인")
-    @Test
-    void noUserSave() {
-        Long userId = 1L;
-        Long scheduleId = 2L;
-        ProceedingRequestDto requestDto = mock(ProceedingRequestDto.class);
-        List<MultipartFile> proceedingImages = Collections.emptyList();
-
-        when(userRepository.findById(userId)).thenReturn(Optional.empty());
-
-        assertThatExceptionOfType(CustomExceptions.class)
-                .isThrownBy(() -> proceedingService.save(userId, scheduleId, requestDto, proceedingImages));
-
-        verify(userRepository).findById(userId);
-        verify(proceedingRepository, never()).save(any(Proceeding.class));
-    }
-
     @DisplayName("업무일지 조회: 작성자가 작성한 모든 업무일지 조회 확인")
     @Test
     void getProceedings() {
-        Long userId = 1L;
-        Long scheduleId = 2L;
         Pageable pageable = PageRequest.of(0, 10);
 
         Proceeding mockProceeding1 = mock(Proceeding.class);
@@ -133,14 +131,9 @@ public class ProceedingServiceTest {
     @DisplayName("업무일지 상세 조회: 업무일지 상세 정보 조회 확인")
     @Test
     void getProceedingDetails() {
-        Long userId = 1L;
-        Long proceedingId = 1L;
-
-        User mockUser = mock(User.class);
         when(mockUser.getId()).thenReturn(userId);
-
-        Proceeding mockProceeding = mock(Proceeding.class);
         when(mockProceeding.getId()).thenReturn(proceedingId);
+        when(mockProceeding.getSchedule()).thenReturn(mockSchedule);
         when(mockProceeding.getUser()).thenReturn(mockUser);
 
         ProceedingImage mockProceedingImage = mock(ProceedingImage.class);
@@ -165,30 +158,21 @@ public class ProceedingServiceTest {
     @DisplayName("존재하지 않는 업무일지의 상세정보 조회 시 예외 발생")
     @Test
     void getClassLogDetailException() {
-        Long userId = 1L;
         Long proceedingId = 100L;
 
         when(proceedingRepository.findByIdAndUserId(proceedingId, userId)).thenReturn(Optional.empty());
 
         assertThatThrownBy(() -> proceedingService.getProceedingDetails(userId, proceedingId))
-                .isInstanceOf(CustomExceptions.class);
+                .isInstanceOf(ProceedingException.class);
     }
 
     @DisplayName("업무일지 삭제: 업무일지 삭제 작업 확인")
     @Test
     void deleteProceeding() {
-        Long userId = 1L;
-        Long proceedingId = 1L;
-
-        Proceeding mockProceeding = mock(Proceeding.class);
         when(mockProceeding.getId()).thenReturn(proceedingId);
-
         when(proceedingRepository.findByIdAndUserId(userId, proceedingId)).thenReturn(Optional.of(mockProceeding));
 
         ProceedingDeleteResponseDto result = proceedingService.deleteProceeding(userId, proceedingId);
-
-        assertThat(result).isNotNull();
-        assertThat(result.getId()).isEqualTo(proceedingId);
 
         verify(proceedingRepository).findByIdAndUserId(userId, proceedingId);
         verify(proceedingRepository).delete(mockProceeding);
@@ -197,13 +181,11 @@ public class ProceedingServiceTest {
     @DisplayName("업무일지 수정: 요청된 값에 따른 업무일지 수정 확인")
     @Test
     void updateClassLog() {
-        Long userId = 1L;
-        Long proceedingId = 1L;
-        Proceeding mockProceeding = mock(Proceeding.class);
         ProceedingUpdateRequestDto proceedingUpdateRequestDto = mock(ProceedingUpdateRequestDto.class);
         List<MultipartFile> proceedingImages = Collections.emptyList();
 
         when(proceedingRepository.findByIdAndUserId(userId, proceedingId)).thenReturn(Optional.of(mockProceeding));
+        when(mockProceeding.getSchedule()).thenReturn(mockSchedule);
 
         ProceedingResponseDto result = proceedingService.updateProceeding(userId, proceedingId,
                 proceedingUpdateRequestDto,
