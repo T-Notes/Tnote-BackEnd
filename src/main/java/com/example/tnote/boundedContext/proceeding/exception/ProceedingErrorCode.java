ed83b592237953281839b134ef74c69f20f084c8
hwan2-99 <thh1233123@gmail.com>
2024-07-26 01:11:46 +0900

    feat: 업무일지 관련 에러메시지 값 추가 (#211)

--- src/main/java/com/example/tnote/boundedContext/proceeding/exception/ProceedingErrorCode.java
+++ src/main/java/com/example/tnote/boundedContext/proceeding/exception/ProceedingErrorCode.java
@@ -6,6 +6,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ProceedingErrorCode implements ErrorCode {
+    PROCEEDING_NOT_FOUNT(HttpStatus.NOT_FOUND, "업무일지를 찾을 수 없습니다."),
+    INVALID_PROCEEDING_DATE(HttpStatus.BAD_REQUEST, "해당 업무일지 내의 기간이 아닙니다."),
+    INVALID_PROCEEDING_DATA(HttpStatus.BAD_REQUEST, "업무일지에 적합한 내용이 아닙니다.")
+    ;
     private final HttpStatus httpStatus;
     private final String message;
 
