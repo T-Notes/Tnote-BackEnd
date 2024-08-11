dbf66990570387335d35f291324ba064adc48955
hwan2-99 <thh1233123@gmail.com>
2024-08-11 12:12:31 +0900

    feat: 교사 일정관리 컨트롤러 추가(#228)

---
+++ src/main/java/com/example/tnote/boundedContext/plan/controller/PlanController.java
@@ -0,0 +1,10 @@
+package com.example.tnote.boundedContext.plan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tnote/v1/plan")
+public class PlanController {
+
+}
