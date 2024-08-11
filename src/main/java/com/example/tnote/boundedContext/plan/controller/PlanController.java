c8e98f382058c5518631cf722b66809928777a06
hwan2-99 <thh1233123@gmail.com>
2024-08-11 13:12:51 +0900

    feat: 일정 추가 api 구현(#228)

--- src/main/java/com/example/tnote/boundedContext/plan/controller/PlanController.java
+++ src/main/java/com/example/tnote/boundedContext/plan/controller/PlanController.java
@@ -1,10 +1,38 @@
 package com.example.tnote.boundedContext.plan.controller;
 
+import com.example.tnote.base.response.Result;
+import com.example.tnote.boundedContext.plan.dto.PlanSaveRequest;
+import com.example.tnote.boundedContext.plan.service.PlanService;
+import com.example.tnote.boundedContext.user.entity.auth.PrincipalDetails;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/tnote/v1/plan")
 public class PlanController {
+
+    private final PlanService planService;
+
+    public PlanController(PlanService planService) {
+        this.planService = planService;
+    }
+
+    @PostMapping(value = "/{scheduleId}", consumes = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Result> save(@AuthenticationPrincipal final PrincipalDetails principalDetails,
+                                       @PathVariable final Long scheduleId,
+                                       @RequestPart final PlanSaveRequest planSaveRequest,
+                                       @RequestPart(name = "planImages", required = false) final List<MultipartFile> planImages) {
+        return ResponseEntity.ok(
+                Result.of(planService.save(principalDetails.getId(), scheduleId, planSaveRequest, planImages)));
+    }
 
 }
