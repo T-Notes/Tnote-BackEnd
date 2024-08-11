3d5de2fd63f91e1e5467d17165485983c9c003bf
hwan2-99 <thh1233123@gmail.com>
2024-08-11 12:27:35 +0900

    refactor: 교사 일정 참석자 엔티티에 baseEntity 상속(#228)

--- src/main/java/com/example/tnote/boundedContext/plan/entity/Participant.java
+++ src/main/java/com/example/tnote/boundedContext/plan/entity/Participant.java
@@ -1,5 +1,6 @@
 package com.example.tnote.boundedContext.plan.entity;
 
+import com.example.tnote.base.entity.BaseTimeEntity;
 import com.example.tnote.boundedContext.user.entity.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "PARTICIPANT")
-public class Participant {
+public class Participant extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
