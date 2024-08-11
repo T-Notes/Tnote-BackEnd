40e7496ac14f0c2486f8e64187d9b1c37d5340e2
hwan2-99 <thh1233123@gmail.com>
2024-08-11 12:08:13 +0900

    feat: 교사일정 작성 요청 dto 추가(#228)

---
+++ src/main/java/com/example/tnote/boundedContext/plan/dto/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.example.tnote.boundedContext.plan.dto;
+
+import com.example.tnote.boundedContext.plan.entity.Plan;
+import com.example.tnote.boundedContext.schedule.entity.Schedule;
+import com.example.tnote.boundedContext.user.entity.User;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import lombok.Getter;
+
+@Getter
+public class RegisterRequest {
+    private String title;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String location;
+    private String contents;
+    //todo 참석자 필드 어떻게 요청받을지 정해지면 필드 추가 and 추후에 추가될 기능이라고함
+
+    public Plan toEntity(User user, Schedule schedule) {
+        return new Plan(this.title, this.startDate, this.endDate, this.location, user, schedule,
+                new ArrayList<>());
+    }
+}
