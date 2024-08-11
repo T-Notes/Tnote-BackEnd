7c9184cb692376a1596ac806c070c80d2901c67f
hwan2-99 <thh1233123@gmail.com>
2024-08-11 12:10:12 +0900

    feat: UserRepository에 Id로 유저 찾는 메소드 default 제한자로 선언(#228)

--- src/main/java/com/example/tnote/boundedContext/user/repository/UserRepository.java
+++ src/main/java/com/example/tnote/boundedContext/user/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package com.example.tnote.boundedContext.user.repository;
 
 import com.example.tnote.boundedContext.user.entity.User;
+import com.example.tnote.boundedContext.user.exception.UserErrorCode;
+import com.example.tnote.boundedContext.user.exception.UserException;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    default User findUserById(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+    }
 }
