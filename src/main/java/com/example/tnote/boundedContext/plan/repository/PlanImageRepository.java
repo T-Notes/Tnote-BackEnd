c5e72a108c8a2f91e1c7f5deb30e345a0a757280
hwan2-99 <thh1233123@gmail.com>
2024-08-14 01:09:45 +0900

    feat: 일정 이미지 삭제 쿼리 작성(#234)

--- src/main/java/com/example/tnote/boundedContext/plan/repository/PlanImageRepository.java
+++ src/main/java/com/example/tnote/boundedContext/plan/repository/PlanImageRepository.java
@@ -1,7 +1,13 @@
 package com.example.tnote.boundedContext.plan.repository;
 
 import com.example.tnote.boundedContext.plan.entity.PlanImage;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlanImageRepository extends JpaRepository<PlanImage, Long> {
+    @Modifying
+    @Query("DELETE FROM PlanImage p WHERE p.plan.id = :planId")
+    void deleteByPlanId(@Param("planId") Long planId);
 }
