3324c8647487142e7d743828718baf28674a3d69
hwan2-99 <thh1233123@gmail.com>
2024-08-14 00:50:25 +0900

    feat: 일정 전체조회 api 구현(#234)

--- src/main/java/com/example/tnote/boundedContext/plan/controller/PlanController.java
+++ src/main/java/com/example/tnote/boundedContext/plan/controller/PlanController.java
@@ -5,12 +5,15 @@ import com.example.tnote.boundedContext.plan.dto.PlanSaveRequest;
 import com.example.tnote.boundedContext.plan.service.PlanService;
 import com.example.tnote.boundedContext.user.entity.auth.PrincipalDetails;
 import java.util.List;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,6 +36,16 @@ public class PlanController {
                                        @RequestPart(name = "planImages", required = false) final List<MultipartFile> planImages) {
         return ResponseEntity.ok(
                 Result.of(planService.save(principalDetails.getId(), scheduleId, planSaveRequest, planImages)));
+    }
+
+    @GetMapping(value = "/{scheduleId}/all")
+    public ResponseEntity<Result> findAll(@AuthenticationPrincipal final PrincipalDetails principalDetails,
+                                          @PathVariable final Long scheduleId,
+                                          @RequestParam(value = "page", required = false, defaultValue = "0") int page,
+                                          @RequestParam(value = "size", required = false, defaultValue = "4") int size) {
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return ResponseEntity.ok(Result.of(planService.findAll(principalDetails.getId(), scheduleId, pageRequest)));
     }
 
 }
