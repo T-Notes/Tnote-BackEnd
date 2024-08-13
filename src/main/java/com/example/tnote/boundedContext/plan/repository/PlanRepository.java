160a796a19cc3e7901271e9f9bbcea591a6c9256
hwan2-99 <thh1233123@gmail.com>
2024-08-14 01:09:39 +0900

    feat: 일정 조회 쿼리 작성(#234)

--- src/main/java/com/example/tnote/boundedContext/plan/repository/PlanRepository.java
+++ src/main/java/com/example/tnote/boundedContext/plan/repository/PlanRepository.java
@@ -2,12 +2,17 @@ package com.example.tnote.boundedContext.plan.repository;
 
 import com.example.tnote.boundedContext.plan.entity.Plan;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+    @Query("select p from Plan p where p.id = :id and p.user.id = :userId")
+    Optional<Plan> findByIdAndUserId(Long id , Long userId);
+
     @Query("select p from Plan p where p.user.id = :userId and p.schedule.id = :scheduleId")
     List<Plan> findALLByUserIdAndScheduleId(Long userId, Long scheduleId);
 
