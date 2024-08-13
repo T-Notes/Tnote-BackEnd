85bc7cdc2081fc0550e365a45fae409d65dc889f
hwan2-99 <thh1233123@gmail.com>
2024-08-14 00:40:24 +0900

    feat: 일정 리스트 응답 객체 추가(#234)

---
+++ src/main/java/com/example/tnote/boundedContext/plan/dto/PlanResponses.java
@@ -0,0 +1,37 @@
+package com.example.tnote.boundedContext.plan.dto;
+
+import com.example.tnote.boundedContext.plan.entity.Plan;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.domain.Slice;
+
+@Getter
+@Builder
+public class PlanResponses {
+    private List<PlanResponse> plans;
+    private int numberOfClassLog;
+    private long page;
+    @JsonProperty(value = "isLast")
+    private Boolean isLast;
+
+    public PlanResponses() {
+    }
+
+    public PlanResponses(List<PlanResponse> plans, int numberOfClassLog, long page, Boolean isLast) {
+        this.plans = plans;
+        this.numberOfClassLog = numberOfClassLog;
+        this.page = page;
+        this.isLast = isLast;
+    }
+
+    public static PlanResponses of(final List<PlanResponse> planResponses, final List<Plan> plans, Slice<Plan> slice) {
+        return PlanResponses.builder()
+                .plans(planResponses)
+                .numberOfClassLog(plans.size())
+                .page(slice.getPageable().getPageNumber())
+                .isLast(slice.isLast())
+                .build();
+    }
+}
