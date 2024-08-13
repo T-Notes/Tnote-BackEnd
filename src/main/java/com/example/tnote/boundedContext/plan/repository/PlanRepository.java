6683f67db929fc6010c857606a6f697b24373b3a
hwan2-99 <thh1233123@gmail.com>
2024-08-14 00:31:57 +0900

    feat: 일정 전체조회 slice 쿼리 작성(#234)

--- src/main/java/com/example/tnote/boundedContext/plan/repository/PlanRepository.java
+++ src/main/java/com/example/tnote/boundedContext/plan/repository/PlanRepository.java
@@ -2,10 +2,15 @@ package com.example.tnote.boundedContext.plan.repository;
 
 import com.example.tnote.boundedContext.plan.entity.Plan;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
     @Query("select p from Plan p where p.user.id = :userId and p.schedule.id = :scheduleId")
-    List<Plan> findByUserIdAndScheduleId(Long userId, Long scheduleId);
+    List<Plan> findALLByUserIdAndScheduleId(Long userId, Long scheduleId);
+
+    @Query("select p from Plan p where p.user.id = :userId and p.schedule.id = :scheduleId ORDER BY p.createdAt DESC")
+    Slice<Plan> findALLByUserIdAndScheduleId(Long userId, Long scheduleId, Pageable pageable);
 }
