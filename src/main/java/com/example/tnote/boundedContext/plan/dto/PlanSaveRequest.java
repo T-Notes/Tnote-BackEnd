c71d0ec1153f065cb42f42ba99ab64121df60810
hwan2-99 <thh1233123@gmail.com>
2024-08-15 00:05:06 +0900

    refactor: 일정 추가 요청 dto클래스에 색상 필드 추가(#239)

--- src/main/java/com/example/tnote/boundedContext/plan/dto/PlanSaveRequest.java
+++ src/main/java/com/example/tnote/boundedContext/plan/dto/PlanSaveRequest.java
@@ -15,21 +15,22 @@ public class PlanSaveRequest {
     private String location;
     private String contents;
     private String participants;
+    private String color;
     //todo 참석자 필드 어떻게 요청받을지 정해지면 필드 추가 and 추후에 추가될 기능이라고함
 
     public Plan toEntity(User user, Schedule schedule) {
         return new Plan(this.title, this.startDate, this.endDate, this.contents, this.location, this.participants,
-                user, schedule, new ArrayList<>());
+                this.color, user, schedule, new ArrayList<>());
     }
 
     public PlanSaveRequest(String title, LocalDateTime startDate, LocalDateTime endDate, String location,
-                           String contents,
-                           String participants) {
+                           String contents, String participants, String color) {
         this.title = title;
         this.startDate = startDate;
         this.endDate = endDate;
         this.location = location;
         this.contents = contents;
         this.participants = participants;
+        this.color = color;
     }
 }
