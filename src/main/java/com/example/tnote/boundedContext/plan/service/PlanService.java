0511ff3e6bf606a96227662be147587ed1de8f46
hwan2-99 <thh1233123@gmail.com>
2024-08-15 12:46:11 +0900

    feat: 일정 수정메서드 구현 (#238)

--- src/main/java/com/example/tnote/boundedContext/plan/service/PlanService.java
+++ src/main/java/com/example/tnote/boundedContext/plan/service/PlanService.java
@@ -5,6 +5,7 @@ import com.example.tnote.boundedContext.plan.dto.PlanDeleteResponse;
 import com.example.tnote.boundedContext.plan.dto.PlanResponses;
 import com.example.tnote.boundedContext.plan.dto.PlanSaveRequest;
 import com.example.tnote.boundedContext.plan.dto.PlanResponse;
+import com.example.tnote.boundedContext.plan.dto.PlanUpdateRequest;
 import com.example.tnote.boundedContext.plan.entity.Plan;
 import com.example.tnote.boundedContext.plan.entity.PlanImage;
 import com.example.tnote.boundedContext.plan.exception.PlanErrorCode;
@@ -83,6 +84,34 @@ public class PlanService {
         return PlanResponse.from(plan);
     }
 
+    @Transactional
+    public PlanResponse updatePlan(final Long userId, final Long planId, final PlanUpdateRequest updateRequest,
+                                   final List<MultipartFile> planImages) {
+        Plan plan = findByIdAndUserId(planId, userId);
+
+        plan.updateFields(
+                updateRequest.getTitle(),
+                updateRequest.getStartDate(),
+                updateRequest.getEndDate(),
+                updateRequest.getLocation(),
+                updateRequest.getContents(),
+                updateRequest.getParticipants()
+        );
+        updateImage(plan, planImages);
+
+        return PlanResponse.from(plan);
+    }
+
+    private void updateImage(Plan plan, List<MultipartFile> planImages) {
+        if (planImages == null || planImages.isEmpty()) {
+            deleteExistedImages(plan);
+        }
+        if (planImages != null && !planImages.isEmpty()) {
+            List<PlanImage> updatedImages = deleteExistedImagesAndUploadNewImages(plan, planImages);
+            plan.updateImages(updatedImages);
+        }
+    }
+
     private List<PlanImage> uploadPlanImages(final Plan plan, final List<MultipartFile> planImages) {
         return planImages.stream()
                 .map(file -> awsS3Uploader.upload(file, "plan"))
@@ -101,6 +130,18 @@ public class PlanService {
         deleteS3Images(plan);
     }
 
+    private void deleteExistedImages(final Plan plan) {
+        System.out.println("Deleting existing images for plan ID: " + plan.getId());
+        deleteS3Images(plan);
+        planImageRepository.deleteByPlanId(plan.getId());
+    }
+
+    private List<PlanImage> deleteExistedImagesAndUploadNewImages(final Plan plan,
+                                                                  final List<MultipartFile> planImages) {
+        deleteExistedImages(plan);
+        return uploadImages(plan, planImages);
+    }
+
     private void deleteS3Images(final Plan plan) {
         System.out.println("Starting to delete images from S3 for plan ID: " + plan.getId());
         List<PlanImage> planImages = plan.getPlanImages();
@@ -109,6 +150,13 @@ public class PlanService {
             System.out.println("Deleting image from S3: " + imageKey);
             awsS3Uploader.deleteImage(imageKey);
         }
+    }
+
+    private List<PlanImage> uploadImages(final Plan plan, final List<MultipartFile> planImages) {
+        return planImages.stream()
+                .map(file -> awsS3Uploader.upload(file, "plan"))
+                .map(pair -> createPlanImage(plan, pair.getFirst(), pair.getSecond()))
+                .toList();
     }
 
     private List<PlanResponse> convertToPlanResponseList(final Slice<Plan> planSlice) {
