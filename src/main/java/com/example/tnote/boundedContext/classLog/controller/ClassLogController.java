c9341a584708164cc2d7907e16e84c33dd041a8b
hwan2-99 <thh1233123@gmail.com>
2024-08-17 13:19:22 +0900

    refactor: 학급일지 수정 컨트롤러 메서드명 수정 및 url 변경(#248)

--- src/main/java/com/example/tnote/boundedContext/classLog/controller/ClassLogController.java
+++ src/main/java/com/example/tnote/boundedContext/classLog/controller/ClassLogController.java
@@ -2,10 +2,9 @@ package com.example.tnote.boundedContext.classLog.controller;
 
 
 import com.example.tnote.base.response.Result;
-import com.example.tnote.boundedContext.classLog.dto.ClassLogDetailResponseDto;
 import com.example.tnote.boundedContext.classLog.dto.ClassLogSaveRequest;
 import com.example.tnote.boundedContext.classLog.dto.ClassLogResponse;
-import com.example.tnote.boundedContext.classLog.dto.ClassLogUpdateRequestDto;
+import com.example.tnote.boundedContext.classLog.dto.ClassLogUpdateRequest;
 import com.example.tnote.boundedContext.classLog.service.ClassLogService;
 import com.example.tnote.boundedContext.user.entity.auth.PrincipalDetails;
 import java.util.List;
@@ -17,7 +16,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,8 +33,7 @@ public class ClassLogController {
         this.classLogService = classLogService;
     }
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,
-            MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<Result> save(@AuthenticationPrincipal final PrincipalDetails principalDetails,
                                        @RequestParam final Long scheduleId,
                                        @RequestPart final ClassLogSaveRequest classLogRequestDto,
@@ -74,15 +71,14 @@ public class ClassLogController {
                 classLogId)));
     }
 
-    @PatchMapping(value = "/{classLogId}", consumes = {MediaType.APPLICATION_JSON_VALUE,
-            MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<Result> updateClassLog(@AuthenticationPrincipal PrincipalDetails principalDetails,
-                                                 @PathVariable Long classLogId,
-                                                 @RequestPart ClassLogUpdateRequestDto classLogUpdateRequestDto,
-                                                 @RequestPart(name = "classLogImages", required = false) List<MultipartFile> classLogImages) {
-        ClassLogResponse classLogResponseDto = classLogService.updateClassLog(principalDetails.getId(), classLogId,
-                classLogUpdateRequestDto, classLogImages);
-        return ResponseEntity.ok(Result.of(classLogResponseDto));
+    @PatchMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Result> update(@AuthenticationPrincipal final PrincipalDetails principalDetails,
+                                                 @RequestParam final Long classLogId,
+                                                 @RequestPart final ClassLogUpdateRequest classLogUpdateRequestDto,
+                                                 @RequestPart(name = "classLogImages", required = false) final List<MultipartFile> classLogImages) {
+
+        return ResponseEntity.ok(Result.of(classLogService.updateClassLog(principalDetails.getId(), classLogId,
+                classLogUpdateRequestDto, classLogImages)));
     }
 
 }
