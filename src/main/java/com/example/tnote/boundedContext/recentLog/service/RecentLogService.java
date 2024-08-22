d0783450a6371d2b0232d90469db4ed120cc746b
hwan2-99 <thh1233123@gmail.com>
2024-08-23 01:03:50 +0900

    refactor: 미사용 객체 삭제(#262)

--- src/main/java/com/example/tnote/boundedContext/recentLog/service/RecentLogService.java
+++ src/main/java/com/example/tnote/boundedContext/recentLog/service/RecentLogService.java
@@ -3,29 +3,18 @@ package com.example.tnote.boundedContext.recentLog.service;
 import com.example.tnote.boundedContext.recentLog.dto.RecentLogResponse;
 import com.example.tnote.boundedContext.recentLog.entity.RecentLog;
 import com.example.tnote.boundedContext.recentLog.repository.RecentLogRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Instant;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class RecentLogService {
-    private static final String RECENT_LOGS_KEY_PREFIX = "recentLogs:";
-    private static final int MAX_RECENT_LOGS = 4;
-
-    private final RedisTemplate<String, String> redisTemplate;
     private final RecentLogRepository recentLogRepository;
-    private final ObjectMapper objectMapper;
 
-    public RecentLogService(final RedisTemplate<String, String> redisTemplate,
-                            final RecentLogRepository recentLogRepository,
-                            final ObjectMapper objectMapper) {
-        this.redisTemplate = redisTemplate;
+    public RecentLogService(final RecentLogRepository recentLogRepository) {
         this.recentLogRepository = recentLogRepository;
-        this.objectMapper = objectMapper;
     }
 
 
