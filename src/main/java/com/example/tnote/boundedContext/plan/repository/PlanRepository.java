370522f9c04ddcf1ca6f86821f88e6988451c545
hwan2-99 <thh1233123@gmail.com>
2024-08-24 01:17:07 +0900

    feat: 일별 일정 조회 메서드 구현(#262)

--- src/main/java/com/example/tnote/boundedContext/plan/repository/PlanRepository.java
+++ src/main/java/com/example/tnote/boundedContext/plan/repository/PlanRepository.java
@@ -1,6 +1,7 @@
 package com.example.tnote.boundedContext.plan.repository;
 
 import com.example.tnote.boundedContext.plan.entity.Plan;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
@@ -11,11 +12,20 @@ import org.springframework.data.jpa.repository.Query;
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
     @Query("select p from Plan p where p.id = :id and p.user.id = :userId")
-    Optional<Plan> findByIdAndUserId(Long id , Long userId);
+    Optional<Plan> findByIdAndUserId(Long id, Long userId);
 
     @Query("select p from Plan p where p.user.id = :userId and p.schedule.id = :scheduleId")
     List<Plan> findALLByUserIdAndScheduleId(Long userId, Long scheduleId);
 
     @Query("select p from Plan p where p.user.id = :userId and p.schedule.id = :scheduleId ORDER BY p.createdAt DESC")
     Slice<Plan> findALLByUserIdAndScheduleId(Long userId, Long scheduleId, Pageable pageable);
+
+    @Query("SELECT p FROM Plan p "
+            + "WHERE p.user.id = :userId AND p.schedule.id = :scheduleId "
+            + "AND p.startDate <= :endOfDay AND p.endDate >= :startOfDay")
+    List<Plan> findByUserIdAndScheduleIdAndStartDateBetween(
+            Long userId,
+            Long scheduleId,
+            LocalDateTime startOfDay,
+            LocalDateTime endOfDay);
 }
